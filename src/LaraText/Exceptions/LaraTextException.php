2d4c3a4bf7c2940aa68cf7f176c8993c512c0aa3
santhoshkorukonda <santhoshkorukonda98@gmail.com>
2017-01-18 16:59:28 +0530

    new-file-structure-added

---
+++ src/LaraText/Exceptions/LaraTextException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace LaraText\Exceptions;
+
+use Exception;
+
+class LaraTextException extends Exception
+{
+    public function __construct($message, $code = 0, Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
--- src/LaraText/helpers/helperfunction.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (!function_exists("sms")) {
-    function sms($phone, string $message)
-    {
-        return SMS::to($phone)->message($message)->send();
-    }
-}
